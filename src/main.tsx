bc4f1d4d6b43874c409bda2fd24d2901bc9baf86
benhengeveld <benhengeveld02@gmail.com>
2025-04-09 15:32:34 -0400

    Update src path to be @

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 
-import App from "./routes/Home.tsx";
-import NotFound from "./routes/NotFound.tsx";
-import "./index.css";
+import App from "@/routes/Home.tsx";
+import NotFound from "@/routes/NotFound.tsx";
+import "@/index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
