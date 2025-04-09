2f2eab555ae7bf0a5fda7abda95aefd4d808ed34
benhengeveld <benhengeveld02@gmail.com>
2025-04-09 10:51:00 -0400

    Add react router

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./Home.css";
 
-function App() {
+export default function App() {
 	const [count, setCount] = useState(0);
 
 	return (
@@ -35,5 +35,3 @@ function App() {
 		</>
 	);
 }
-
-export default App;
