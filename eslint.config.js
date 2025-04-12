6089dd4933f3ccd33bc6216623020c065d5bd52c
benhengeveld <benhengeveld02@gmail.com>
2025-04-12 11:52:59 -0400

    fresh

--- eslint.config.js
+++ eslint.config.js
@@ -1,63 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-plugin-prettier";
-import importPlugin from "eslint-plugin-import";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-	{ ignores: ["dist"] },
-	{
-		extends: [js.configs.recommended, ...tseslint.configs.recommended],
-		files: ["**/*.{ts,tsx}"],
-		languageOptions: {
-			ecmaVersion: 2020,
-			globals: globals.browser,
-		},
-		plugins: {
-			"react-hooks": reactHooks,
-			"react-refresh": reactRefresh,
-			import: importPlugin,
-			prettier,
-		},
-		rules: {
-			...reactHooks.configs.recommended.rules,
-			"react-refresh/only-export-components": [
-				"warn",
-				{ allowConstantExport: true },
-			],
-			"prettier/prettier": "error",
-			"import/order": [
-				"error",
-				{
-					groups: [
-						"builtin",
-						"external",
-						"internal",
-						"parent",
-						"sibling",
-						"index",
-						"object",
-						"type",
-					],
-					pathGroups: [
-						{
-							pattern: "react",
-							group: "external",
-							position: "before",
-						},
-					],
-					pathGroupsExcludedImportTypes: ["react"],
-					"newlines-between": "always",
-					alphabetize: {
-						order: "asc",
-						caseInsensitive: true,
-					},
-				},
-			],
-			"import/newline-after-import": ["error", { count: 1 }],
-			"import/no-duplicates": "error",
-		},
-	},
-);
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import App from "@/routes/Home.tsx";
-import NotFound from "@/routes/NotFound.tsx";
-import "@/index.css";
-
-createRoot(document.getElementById("root")!).render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />} />
-			<Route path="*" element={<NotFound />} />
-		</Routes>
-	</BrowserRouter>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
