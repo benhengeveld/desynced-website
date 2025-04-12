675652389b76872579771d76ced43f13d3dfa6cc
benhengeveld <benhengeveld02@gmail.com>
2025-04-12 12:10:50 -0400

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+// eslint.config.mjs
+import antfu from "@antfu/eslint-config";
+
+export default antfu(
+	{
+		type: "app",
+		typescript: true,
+		formatters: true,
+		stylistic: {
+			indent: "tab",
+			semi: true,
+			quotes: "double",
+		},
+	},
+	{
+		rules: {
+			"ts/no-redeclare": "off",
+			"ts/consistent-type-definitions": ["error", "type"],
+			"no-console": ["warn"],
+			"antfu/no-top-level-await": ["off"],
+			"node/prefer-global/process": ["off"],
+			"node/no-process-env": ["error"],
+			"perfectionist/sort-imports": [
+				"error",
+				{
+					tsconfigRootDir: ".",
+				},
+			],
+		},
+	},
+);
