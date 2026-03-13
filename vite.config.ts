684ded13ce693d43233a7e68adb228b64de6b509
benhengeveld <benhengeveld02@gmail.com>
2026-03-13 19:58:35 -0400

    init svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
-import path from "node:path";
+import devtoolsJson from 'vite-plugin-devtools-json';
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-});
+export default defineConfig({ plugins: [tailwindcss(), sveltekit(), devtoolsJson()] });
